c9444cce28c6beff561d9bb5854d6d2cf983de4b
Nicalo Ribeiro <nicalo63@gmail.com>
2023-11-13 20:03:18 -0300

    Removendo elementos nao utilizados

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,7 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import NavBar from "./components/NavBar";
-import { Layout, Space } from "antd";
 import { Outlet } from "react-router-dom";
-import EmptyMessage from "./components/EmptyMessage";
-const { Header, Content, Footer, Sider } = Layout;
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <div className="lg:flex w-screen h-screen">
       <NavBar />
