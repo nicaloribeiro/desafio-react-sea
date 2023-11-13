ae0b34f29e2b7db0daaeec4bcd297feae3d6c10e
Nicalo Ribeiro <nicalo63@gmail.com>
2023-11-13 17:53:23 -0300

    Configuracao inicial das rotas

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,13 +1,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './styles/index.css'
 import { ToastContainer } from 'react-toastify';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import './styles/index.css'
 import 'react-toastify/dist/ReactToastify.css';
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+]);
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
     <ToastContainer/>
   </React.StrictMode>,
 )
