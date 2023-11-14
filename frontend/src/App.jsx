2773bcbf6fe838c4a540d015da867e577784fecc
Nicalo Ribeiro <nicalo63@gmail.com>
2023-11-14 12:35:43 -0300

    Ajustes na estilizacao base

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,9 +3,9 @@ import { Outlet } from "react-router-dom";
 
 function App() {
   return (
-    <div className="lg:flex w-screen h-screen">
+    <div className="lg:flex w-screen lg:h-screen">
       <NavBar />
-      <div className="h-full w-full px-8 pt-8">
+      <div className="h-full w-full px-8 pt-8 overflow-y-auto">
         <Outlet />
       </div>
     </div>
