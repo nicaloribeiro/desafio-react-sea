db2699e5873be8e9435b40bc4347f0115b151c7a
Nicalo Ribeiro <nicalo63@gmail.com>
2023-11-15 15:20:39 -0300

    Criacao e configuracao do service da API

---
+++ frontend/src/services/employees/index.js
@@ -0,0 +1,29 @@
+import api from "../api-config";
+
+const getAll = async () => {
+  const employees = await api.get("/employees");
+  const { data } = employees;
+  return data;
+};
+
+const create = async (employee) => {
+  const created = await api.post("/employees", employee);
+  const { data } = created;
+  return data;
+};
+
+const update = async (employee) => {
+  const { id } = employee;
+  const upadated = await api.put(`/employees/${id}`, employee);
+  const { data } = upadated;
+  console.log("data", data);
+  return data;
+};
+
+const employeesService = {
+  getAll,
+  create,
+  update,
+};
+
+export default employeesService;
