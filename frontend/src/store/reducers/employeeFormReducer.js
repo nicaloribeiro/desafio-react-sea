a374aaa6afc587e6e553f5f32af254d7128667ea
Nicalo Ribeiro <nicalo63@gmail.com>
2023-11-15 20:59:47 -0300

    Remocao de codigo comentado

--- frontend/src/store/reducers/employeeFormReducer.js
+++ frontend/src/store/reducers/employeeFormReducer.js
@@ -51,40 +51,6 @@ const employeeFormSlice = createSlice({
   },
 });
 
-// const employerFormReducer = (state = initialState, action) => {
-//   switch (action.type) {
-//     case RESET_FORM:
-//       return initialState;
-//     case SET_FORM:
-//       return action.payload.employee;
-//     case UPDATE_FIELD:
-//       return {
-//         ...state,
-//         [action.payload.field]: action.payload.value,
-//       };
-//     case UPDATE_ACTIVITY:
-//       return {
-//         ...state,
-//         activities: handleChangeActivities(
-//           state.activities,
-//           action.payload.value
-//         ),
-//       };
-//     case INSERT_ACTIVITY:
-//       return {
-//         ...state,
-//         activities: [...state.activities, action.payload.value],
-//       };
-//     case INSERT_DOCUMENT:
-//       return {
-//         ...state,
-//         document: action.payload.document,
-//       };
-//     default:
-//       return state;
-//   }
-// };
-
 export default employeeFormSlice.reducer;
 export const {
   resetForm,
